528c699025ef879c02be62342eef05a324c543df
Filip Rzepiela <filiprzepiela@interia.pl>
2021-06-01 19:13:57 +0200

    pwir11

---
+++ PWIR_11/Cwiczenie11/Cwiczenie11/App_Code/IService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// UWAGA: możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę interfejsu „IService” w kodzie i pliku konfiguracji.
+[ServiceContract]
+public interface IService
+{
+    [OperationContract]
+    void DoWork();
+
+    [OperationContract]
+    int fib(int a);
+
+    [OperationContract]
+    int silnia(int n);
+
+    [OperationContract]
+    double Pole_prost(double a, double b);
+
+    [OperationContract]
+    string trojkaty(double a, double b, double c);
+
+}
---
+++ PWIR_11/Cwiczenie11/Cwiczenie11/App_Code/Service.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// UWAGA: możesz użyć polecenia „Zmień nazwę” w menu „Refaktoryzuj”, aby zmienić nazwę klasy „Service” w kodzie, usłudze i pliku konfiguracji.
+public class Service : IService
+{
+    public void DoWork()
+    {
+    }
+    public int fib(int a)
+    {
+
+        if (a == 0 || a == 1)
+        {
+            return a;
+        }
+
+        else
+        {
+            return fib(a - 1) + fib(a - 2);
+        }
+
+    }
+    public int silnia(int n)
+    {
+
+        if (n == 0 || n == 1)
+        {
+            return 1;
+        }
+
+        else
+        {
+            return n * silnia(n - 1);
+        }
+
+    }
+
+
+
+    public double Pole_prost(double a, double b)
+    {
+        if (a > 0 && b > 0)
+        {
+
+            return a * b;
+        }
+
+
+        else
+        {
+            return 0;
+        }
+    }
+
+    public string trojkaty(double a, double b, double c)
+    {
+
+        if (a > 0 && b > 0 && c > 0)
+        {
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                return "Jest trójkąt";
+            }
+            else
+            {
+                return "Nie ma trójąta";
+            }
+
+        }
+        else
+        {
+            return "Boki nie mogą być miejsze lub równe 0";
+        }
+
+    }
+
+}
